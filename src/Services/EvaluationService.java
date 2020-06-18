e055f3e02c63d398c79ff0be502fc26139302d56
bergaouifedy <bergaouifedy14@gmail.com>
2020-06-19 00:29:55 +0100

    Add files via upload

--- src/Services/EvaluationService.java
+++ src/Services/EvaluationService.java
@@ -8,17 +8,20 @@ package Services;
 import Entities.Evaluations;
 import Utils.DataBase;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import java.util.ArrayList;
 
 /**
  *
- * @author h^
+ * @author fedi
  */
 public class EvaluationService {
 
@@ -48,11 +51,12 @@ public class EvaluationService {
     }
     
     public void maj(Evaluations e){
-        String req= " UPDATE evaluations SET (note, commentaire)='"+ e.getNote() + "', '" + e.getCommentaire() + "' where id= '"+e.getId()+"' " ;
+       
         Statement st;
         try {
              st = cx.createStatement();
-             st.executeUpdate(req);
+             st.executeUpdate("UPDATE evaluations SET note='"+e.getNote()+"',commentaire='"+e.getCommentaire()+
+                        "' WHERE id= "+e.getId());
                     }   
         catch (SQLException ex) {
             Logger.getLogger(EvaluationService.class.getName()).log(Level.SEVERE, null, ex);
@@ -81,7 +85,7 @@ public class EvaluationService {
             st=cx.createStatement();
             ResultSet resultat=st.executeQuery(req);
             while(resultat.next())
-            {    Evaluations c = new Evaluations(resultat.getInt("note"), resultat.getString("commentaire"), resultat.getString("titre"));
+            {    Evaluations c = new Evaluations(resultat.getString("titre"),resultat.getString("commentaire"),resultat.getInt("note"));
                /*  c.setTitre(resultat.getString(2));
                  c.setDescription(resultat.getString(3));*/
                  mylist.add(c);
@@ -112,6 +116,105 @@ public class EvaluationService {
         }
           return mylist;
      }
+    
+     public List<Evaluations> FindByUserID(int id)  {
+        
+        
+            
+        List<Evaluations> listEV = new ArrayList<>();
+        
+        try {
+        String req = "SELECT * FROM `evaluations` WHERE user_id = ?";
+        PreparedStatement pre = cx.prepareStatement(req);
+        pre.setInt(1,id);
+        ResultSet rs = pre.executeQuery();
+        while (rs.next()) {
+            
+            
+              listEV.add(new Evaluations(
+                         rs.getInt("id"),
+                      rs.getDouble("note"),
+                      rs.getString("commentaire"),
+                         rs.getInt("event_id"),
+                         rs.getInt("user_id")
+                ));
+     
+        }
+        
+              } catch (SQLException ex) {
+            Logger.getLogger(EvaluationService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return listEV;
+    }
+     
+     
+     public ObservableList<Evaluations> afficherEvaluationsparidev(int id) {
+        ObservableList<Evaluations> mylist=FXCollections.observableArrayList();
+            try {
+            
+           
+            PreparedStatement pt = cx.prepareStatement("SELECT ev.note,ev.commentaire,u.username FROM evaluations ev INNER JOIN fos_user u ON ev.user_id=u.id  WHERE ev.event_id = ?");
+            pt.setInt(1, id);
+            ResultSet rs = pt.executeQuery();
+            while (rs.next()) {
+                Evaluations c = new Evaluations(rs.getDouble("note"), rs.getString("commentaire"), rs.getString("username"));
+              //  evt.setIdPro(rs.getInt(10));   
+
+             mylist.add(c);
+                    }
+        } catch (SQLException ex) {
+            System.out.println("erreur " + ex.getMessage());
+        }
+        
+        return mylist;
+    }
+     
+     
+      public Evaluations afficherevaluationparidEventidUser(int event_id, int user_id) {
+         Evaluations  evt= new Evaluations();
+            try {
+            
+           
+            PreparedStatement pt = cx.prepareStatement("select * from evaluations WHERE event_id = ? AND user_id=?");
+            pt.setInt(1, event_id);
+                        pt.setInt(2, user_id);
+
+            ResultSet rs = pt.executeQuery();
+            while (rs.next()) {
+                evt.setId(rs.getInt(1));
+               evt.setEvent_id(rs.getInt(2));
+               evt.setUser_id(rs.getInt(3));
+               evt.setNote(rs.getInt(4));
+               evt.setCommentaire(rs.getString(5));
+              //  evt.setIdPro(rs.getInt(10));   
+
+            }
+        } catch (SQLException ex) {
+            System.out.println("erreur " + ex.getMessage());
+        }
+        
+        return evt;
+    }
+     
+     public double eval(int id) throws SQLException{
+            String query="select avg(note) from evaluations where event_id=?" ;
+            PreparedStatement pre = cx.prepareStatement(query);
+             pre.setInt(1,id);
+             ResultSet rs = pre.executeQuery();
+            while(rs.next()){
+            Double notecours=rs.getDouble("note");
+                return notecours;
+                
+            }
+       return 0;
+    }
+     
+     
+                
+
+
+    
                 
 }
 
--- src/Services/EventService.java
+++ src/Services/EventService.java
@@ -24,10 +24,19 @@ import javafx.collections.ObservableList;
 public class EventService {
 
     Connection cx = DataBase.getInstance().getCnx();
+    
+    public static EventService su;
+    
+     public static EventService getInstance() {
+        if(su == null )
+            su = new EventService();
+        return su;
+            
+    }
 
     public void ajouter(Events e) {
-        String req = "INSERT INTO events (titre, description, date, nbrPlaces, prix, localisation,idPro, Image) VALUES ( '"
-                + e.getTitre() + "', '" + e.getDescription() + "','" + e.getDate() + "','" + e.getNbrPlaces() + "','" + e.getPrix() + "','" + e.getLocalisation() + "','" + e.getIdPro()+  "','" + e.getImage() + "')";
+        String req = "INSERT INTO events (titre, description, date, nbrPlaces, prix, localisation,idPro, nom_image) VALUES ( '"
+                + e.getTitre() + "', '" + e.getDescription() + "','" + e.getDate() + "','" + e.getNbrPlaces() + "','" + e.getPrix() + "','" + e.getLocalisation() + "','" + e.getIdPro()+  "','" + e.getNom_image() + "')";
         Statement st;
         try {
             st = cx.createStatement();
@@ -84,7 +93,7 @@ public class EventService {
             st=cx.createStatement();
             ResultSet resultat=st.executeQuery(req);
             while(resultat.next())
-            {    Events c = new Events(resultat.getInt("id") , resultat.getString("titre"), resultat.getString("description"),resultat.getString ("date"), resultat.getInt("nbrPlaces"), resultat.getDouble("prix"), resultat.getString("localisation"), resultat.getInt ("idPro"), resultat.getString("Image"));
+            {    Events c = new Events(resultat.getInt("id") , resultat.getString("titre"), resultat.getString("description"),resultat.getString ("date"), resultat.getInt("nbrPlaces"), resultat.getDouble("prix"), resultat.getString("localisation"), resultat.getInt ("idPro"), resultat.getString("nom_image"));
                /*  c.setTitre(resultat.getString(2));
                  c.setDescription(resultat.getString(3));*/
                  mylist.add(c);
@@ -119,7 +128,7 @@ public ObservableList<Events>recherche(String titre) {
               Double prix =rs.getDouble("prix");
               String date =rs.getString("date");
               int nbrPlaces= rs.getInt("nbrPlaces");
-                          String nom_image =rs.getString("Image");
+                          String nom_image =rs.getString("nom_image");
 
                Events e = new Events(id, titre, description, prix, date, nbrPlaces, nom_image);
            events.add(e);
@@ -139,6 +148,98 @@ public ObservableList<Events>recherche(String titre) {
         Statement pstm = cx.createStatement();
        pstm.executeUpdate(query);
     }
+    
+        public void increment_nb(int event_id) throws SQLException{
+        PreparedStatement preparedStatement = null ;
+        String query="update events e INNER JOIN reservation r ON e.id=r.event_id set e.nbrPlaces =e.nbrPlaces+r.quantite WHERE event_id="+event_id;
+        Statement pstm = cx.createStatement();
+       pstm.executeUpdate(query);
+    }
+    
+    
+    
+     public void afficher2() {
+        try {
+            PreparedStatement pt = cx.prepareStatement("SELECT * FROM events");
+            ResultSet result = pt.executeQuery();
+            while (result.next()) {
+        System.out.println(result.getInt(1)+"  "+result.getString(2)+"  "+result.getString(3)+" "+result.getString(4)); 
+            }
+        } catch (SQLException ex) {
+            System.out.println("erreur " + ex.getMessage());
+        }
+    }
+    
+    public ResultSet afficherevents() {
+                ResultSet rs = null;
+        try {
+            PreparedStatement pt = cx.prepareStatement("SELECT * FROM events");
+            rs = pt.executeQuery();
+    
+        } catch (SQLException ex) {
+            System.out.println("erreur " + ex.getMessage());
+        }
+            return rs;
+    }
+    
+    
+    
+     public ResultSet afficherComingevents() {
+                ResultSet rs = null;
+        try {
+            PreparedStatement pt = cx.prepareStatement("SELECT * FROM events  WHERE date > CURRENT_DATE()  ");
+            rs = pt.executeQuery();
+    
+        } catch (SQLException ex) {
+            System.out.println("erreur " + ex.getMessage());
+        }
+            return rs;
+    }
+     
+       public ResultSet afficherPastevents() {
+                ResultSet rs = null;
+        try {
+            PreparedStatement pt = cx.prepareStatement("SELECT * FROM events  WHERE date <= CURRENT_DATE()  ");
+            rs = pt.executeQuery();
+    
+        } catch (SQLException ex) {
+            System.out.println("erreur " + ex.getMessage());
+        }
+            return rs;
+    }
+    
+    
+      public Events affichereventparid(int id) {
+         Events  evt= new Events();
+            try {
+            
+           
+            PreparedStatement pt = cx.prepareStatement("select * from events WHERE id = ?");
+            pt.setInt(1, id);
+            ResultSet rs = pt.executeQuery();
+            while (rs.next()) {
+                evt.setId(rs.getInt(1));
+                evt.setTitre(rs.getString(2));
+                evt.setLocalisation(rs.getString(7));
+                evt.setDate(rs.getString(4));
+                evt.setDescription(rs.getString(3));
+                evt.setNbrPlaces(rs.getInt(5));
+                evt.setPrix(rs.getInt(6));
+                evt.setNom_image(rs.getString(8));
+                evt.setIdPro(rs.getInt(9));
+              //  evt.setIdPro(rs.getInt(10));   
+
+            }
+        } catch (SQLException ex) {
+            System.out.println("erreur " + ex.getMessage());
+        }
+        
+        return evt;
+    }
+    
+    
+       
+   
 
 }
 
